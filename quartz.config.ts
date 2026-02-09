ad0d468f267dc1992317191b73d8d553fa146598
Daehyun Kim <kimdhyungg@gmail.com>
2026-02-09 17:27:36 +0900

    rename

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "CUBRID OOS",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
